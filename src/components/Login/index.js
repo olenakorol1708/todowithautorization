f95e13a7199c614f356262bb81aff7f6dfc3f8f0
luckykorol <luckykorol@gmail.com>
2023-04-25 21:17:31 +0300

    second commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+// import { uuid } from 'uuidv4';
    const Login = () => {
-   
-   
+   const [passwordData,setPasswordData] = React.useState([{password:null}]);
+   const [emailData,setEmailData] = React.useState([{email:null}]);
+
+
+   const data =
+    {   
+      "password": "password",
+   "email":"email"}
  async function loginFetch(){
   try{
     const result = await fetch("https://first-node-js-app-r.herokuapp.com/api/auth/login",{
@@ -11,18 +17,14 @@ headers:{
    
      'Content-Type': 'application/json' 
 },
-body: JSON.stringify({
-    
-        "email": "olena1708@gmail.com",
-        "password": "17Aug2023!!"
-      })
-
+body: JSON.stringify(data)
 })
 const data = await result.json();
 console.log(data)
 // localStorage.setItem('access',data.access);
 // localStorage.setItem('refresh',data.refresh)
 // console.log(localStorage);
+// "olena1708@gmail.com"   17Aug2023!!
   }
   catch(erorr){
     console.log(erorr.message)
@@ -35,9 +37,8 @@ loginFetch()
   return (
   <form className='login-form'>
     <h3>Login</h3>
-    <input type = "password"/>
-   
-    <input type = "email"/>
+    <label for = "password">password <input type = "password" onChange={(e)=>setPasswordData(e.target.value)} value = {passwordData}/></label>
+    <label for = "email">email<input type = "email" onChange={(e)=>setEmailData(e.target.value)} value = {emailData}/></label>
     <button>Submit</button>
   </form>
   )
